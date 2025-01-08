2ff2c7998dc82eec238c682da7f0be8e8aafd2d2
aldityanayaka <aldityanayaka@gmail.com>
2025-01-08 07:53:43 +0700

    Menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello world</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus optio vel at molestiae labore recusandae quibusdam. Aspernatur maiores laboriosam hic, eveniet ea quaerat eius iste, neque quod eum obcaecati voluptas.</p>
 </body>
 </html>
